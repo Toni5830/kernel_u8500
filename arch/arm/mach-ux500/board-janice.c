a80d22f1581c04c758ad8b7774636cd31282f7f1
cocafe <cocafehj@gmail.com>
2014-07-05 19:33:38 +0800

    board-janice: Reduce touchscreen threshold
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- arch/arm/mach-ux500/board-janice.c
+++ arch/arm/mach-ux500/board-janice.c
@@ -577,7 +577,7 @@ static const u8 *mxt224_config[] = {
 */
 #define MXT224E_THRESHOLD_BATT			15	/* Default: 22 */
 #define MXT224E_THRESHOLD_CHRG			20	/* Default: 25 */
-#define MXT224E_CALCFG_BATT			0x72 	//114
+#define MXT224E_CALCFG_BATT			0x72 //114
 #define MXT224E_CALCFG_CHRG			0x72 
 #define MXT224E_ATCHFRCCALTHR_NORMAL		40
 #define MXT224E_ATCHFRCCALRATIO_NORMAL		55
