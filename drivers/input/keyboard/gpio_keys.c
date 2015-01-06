793d3876931a3168676f341bc81616510503cdfe
cocafe <cocafehj@gmail.com>
2015-01-06 16:24:31 +0800

    debug: Remove crappy upload mode keys
    Conflicts:
    	drivers/input/keyboard/gpio_keys.c
    	drivers/input/misc/ab8500-ponkey.c

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -26,14 +26,6 @@
 #include <linux/workqueue.h>
 #include <linux/gpio.h>
 #include <linux/pm_runtime.h>
-#ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
-#include <mach/sec_common.h>
-#include <mach/sec_param.h>
-
-struct timer_list debug_timer;
-struct gpio_keys_platform_data *g_pdata;
-extern int jack_is_detected;
-#endif
 
 extern struct class *sec_class;
 
@@ -401,24 +393,6 @@ static struct attribute_group sec_key_attr_group = {
 	.attrs = sec_key_attrs,
 };
 
-#ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
-bool gpio_keys_getstate(int keycode)
-{
-	switch (keycode) {
-	case KEY_VOLUMEUP:
-		return g_bVolUp;
-	case KEY_POWER:
-		return g_bPower;
-	case KEY_HOME:
-		return g_bHome;
-	default:
-		break;
-	}
-	return 0;
-}
-EXPORT_SYMBOL(gpio_keys_getstate);
-#endif //CONFIG_SAMSUNG_PRODUCT_SHIP
-
 void gpio_keys_setstate(int keycode, bool bState)
 {
 	switch (keycode) {
@@ -450,26 +424,6 @@ static void gpio_keys_report_event(struct gpio_button_data *bdata)
 	struct input_dev *input = bdata->input;
 	unsigned int type = button->type ?: EV_KEY;
 	int state = (gpio_get_value_cansleep(button->gpio) ? 1 : 0) ^ button->active_low;
-#if !defined(CONFIG_SAMSUNG_PRODUCT_SHIP)
-	printk(KERN_DEBUG "[KEY] key: %s gpio_keys_report_event state = %d \n",
-		button->desc, state);
-
-	/* Forced Upload Mode checker */
-	bool bState = false;
-
-	bState = state ? true : false;
-
-	switch (button->code) {
-	case KEY_VOLUMEUP:
-		g_bVolUp = bState;
-		break;
-	case KEY_HOME:
-		g_bHome = bState;
-		break;
-	default:
-		break;
-	}
-#endif
 
 	if (type == EV_ABS) {
 		if (state)
@@ -692,11 +646,6 @@ static int __devinit gpio_keys_probe(struct platform_device *pdev)
 	input_sync(input);
 
 	device_init_wakeup(&pdev->dev, wakeup);
-
-#if !defined(CONFIG_SAMSUNG_PRODUCT_SHIP)
-	/* Initialize for Forced Upload mode */
-	g_pdata = pdata;
-#endif
 
 	return 0;
 
